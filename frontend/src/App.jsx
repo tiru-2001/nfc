d07db08bbe4537b07d1962cbdb10671a18953c2b
tiru-2001 <tirumaleshae709@gmail.com>
2024-10-07 17:39:37 +0530

    added new page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,16 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Home from './page/home/Home';
+import Parent from './page/Parent/Parent';
 function App() {
   return (
     <>
       <ToastContainer />
       <Router>
         <Routes>
-          <Route path="api/v1/user/:userId" element={<Home />} />
+          <Route path="/" element={<Parent />} />
+
+          <Route path="/user/:userId" element={<Home />} />
         </Routes>
       </Router>
     </>
---
+++ frontend/src/page/Parent/Parent.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Parent = () => {
+  return <div>Home</div>;
+};
+
+export default Parent;
